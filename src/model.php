13571ec2d3a551833682fe8f536b11a582b4e5a1
nassim90 <laouiti90@yahoo.fr>
2017-02-28 08:39:31 +0100

    modif

---
+++ src/model.php
@@ -0,0 +1,8 @@
+<?php
+
+// Return all articles
+function getBillets() {
+    $bdd = new PDO('mysql:host=localhost;dbname=moncms;charset=utf8', 'root', '');
+    $articles = $bdd->query('select * from billets order by date_creation desc');
+    return $billets;
+}
