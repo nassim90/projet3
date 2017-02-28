c22d88db72d0c6eaad24c297d3dad1ae7448b432
nassim90 <laouiti90@yahoo.fr>
2017-02-28 14:15:51 +0100

    deplace dossier

--- src/model.php
+++ src/model.php
@@ -3,6 +3,6 @@
 // Return all articles
 function getBillets() {
     $bdd = new PDO('mysql:host=localhost;dbname=moncms;charset=utf8', 'root', '');
-    $articles = $bdd->query('select * from billets order by date_creation desc');
+    $billets = $bdd->query('select * from billets order by date_creation desc');
     return $billets;
 }
