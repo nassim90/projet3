84687bf7f0c8e38c48d5fab9072f60532e3090a3
nassim90 <laouiti90@yahoo.fr>
2017-03-20 13:13:21 +0100

    controller

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
 // Register global error and exception handlers
 ErrorHandler::register();
 ExceptionHandler::register();
@@ -54,3 +55,33 @@ $app['dao.comment'] = function ($app) {
     return $commentDAO;
 };
 
+// Register service providers
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/projet3.log',
+    'monolog.name' => 'projet3',
+    'monolog.level' => $app['monolog.level']
+));
+
+// Register error handler
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = "Something went wrong.";
+    }
+    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
+
+// Register JSON data decoder for JSON requests
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
--- app/config/prod.php
+++ app/config/prod.php
@@ -10,3 +10,6 @@ $app['db.options'] = array(
     'user'     => 'root',
     'password' => '',
 );
+
+// define log level
+$app['monolog.level'] = 'WARNING';
